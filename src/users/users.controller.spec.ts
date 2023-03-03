ffc8c80f6b93d5657ba3fff5bef3e46b78c3afd6
Hao Chen <hche8093@uni.sydney.edu.au>
2023-03-03 21:01:14 +1100

    Add unit testingo of users.controller

---
+++ src/users/users.controller.spec.ts
@@ -0,0 +1,53 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { AuthService } from './auth.service';
+import { User } from './user.entity';
+
+describe('UsersController', () => {
+  let controller: UsersController;
+  // Only need to implement partial methods or properties in UsersService
+  let fakeUsersService: Partial<UsersService>;
+  let fakeAuthService: Partial<AuthService>;
+
+  beforeEach(async () => {
+    fakeUsersService = {
+      findOneById: (id: number) => {
+        return Promise.resolve({
+          id,
+          email: 'test@test.com',
+          password: 'test',
+        } as User);
+      },
+      findAllByEmail: (email: string) => {
+        return Promise.resolve([{ id: 1, email, password: 'test' } as User]);
+      },
+      // remove: () => {},
+      // update: () => {},
+    };
+    fakeAuthService = {
+      // signup: () => {},
+      // signin: () => {},
+    };
+
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [UsersController],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: fakeUsersService,
+        },
+        {
+          provide: AuthService,
+          useValue: fakeAuthService,
+        },
+      ],
+    }).compile();
+
+    controller = module.get<UsersController>(UsersController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
