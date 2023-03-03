631cb7e324ff35a6609643767fd644390b3639dc
Hao Chen <hche8093@uni.sydney.edu.au>
2023-03-03 21:37:59 +1100

    Add unit testing of findUser and findAllUsers

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -52,6 +52,17 @@ describe('UsersController', () => {
     expect(controller).toBeDefined();
   });
 
+  it('findAllUsers returns a list of users with the given email', async () => {
+    const users = await controller.findAllUsers('test@test.com');
+    expect(users.length).toEqual(1);
+    expect(users[0].email).toEqual('test@test.com');
+  });
+
+  it('findUser returns a single user with the given id', async () => {
+    const user = await controller.findUser('1');
+    expect(user).toBeDefined();
+  });
+
   it('findUser throws an error if user with given id is not found', async () => {
     fakeUsersService.findOneById = () => null;
     await expect(controller.findUser('1')).rejects.toThrow(NotFoundException);
